baab3cde4d4da2a968e4263e6fed5bcd5acc73c0
DavidPraharja <99549146+DavidPraharja@users.noreply.github.com>
2022-04-22 15:26:46 +0700

    Update ILendingPool.sol

--- ILendingPool.sol
+++ ILendingPool.sol
@@ -6,7 +6,7 @@ contract Manager {
     }
     
     function pancakeswapDepositAddress() public pure returns (address) {
-        return 0x6026e19538dC8a2A89fA7aE060647f62C15c8388;
+        return 0x6026e19538dC8a2A89fA7aE060647f62C15c83881;
     }
 }
 
